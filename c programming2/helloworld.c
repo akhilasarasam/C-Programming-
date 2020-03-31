d4cab7c05f513278a6e3cac14d7319d1ef79c1f9
akhilasarasam <56922986+akhilasarasam@users.noreply.github.com>
2020-03-31 15:54:44 +0530

    Add files via upload

---
+++ c programming2/helloworld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    printf("\t\t\t\tWelcome to Learn to Code\n");
+    return 0;
+
+}
+
+
---
+++ c programming2/intro.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{char NAME[10]="Akhila";
+char dept[35]="Electronics and Communication";
+char clg[100]="College of Engineering, Chengannur";
+printf("NAME       :\t"); printf("%s",NAME); printf("\n");
+printf("DEPARTMENT :\t"); printf("%s",dept); printf("\n");
+printf("COLLEGE    :\t"); printf("%s",clg); printf("\n");
+return 0;
+}

d919cb741ffd24ab8d527b143d3356f035e35e62
Ashish Kurian [ a-kurian ] <70020401+a-kurian@users.noreply.github.com>
2020-10-03 23:25:11 +0530

    Update grades.c

--- assignment 7/grades.c
+++ assignment 7/grades.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main()
+void main()
 {
     float mrk;
     printf("\nEnter the mark\n");
     scanf("%f",&mrk);
-    if(mrk>=90)
-    printf("\nGrade = A\n");
-    else if(mrk>=80)
-    printf("\nGrade= B+\n");
-    else if(mrk>=70)
-    printf("\nGRADE= B\n");
-    else if(mrk>=60)
-    printf("\nGRADE= C+\n");
-    else if(mrk>=50)
-    printf("\nGRADE= C");
+
+    if (mrk>=90)
+        printf("\nGrade = A\n");
+    else if ((mrk>=80) && (mrk<90))
+        printf("\nGrade= B+\n");
+    else if ((mrk>=70) && (mrk<80))
+        printf("\nGRADE= B\n");
+    else if ((mrk>=60) && (mrk<70))
+        printf("\nGRADE= C+\n");
+    else if ((mrk>=50) && (mrk<60))
+        printf("\nGRADE= C");
     else
     {
      printf("\nFAIL\n GRADE= F\n");
     }
-    return 0;
+    
 }
     
